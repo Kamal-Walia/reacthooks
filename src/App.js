d019f0928500ec9a19c220ac5746e8771934dec4
Kamal Walia <kamalw1709@gmail.com>
2022-09-07 17:55:32 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ShowName from './showName'
+
 
 function App() {
+
+  const [fName, setFirstName] = React.useState('')
+  const [lName, updateLastName] = React.useState('')
+  const [age, setAge] = React.useState()
+
+  const handleUpdateFirstName = (e) => {
+      setFirstName(e.target.value)    
+  }
+
+  const HandleUpdateLastName = (e) => {
+    updateLastName(e.target.value)
+  }
+
+  const handleUpdateAge = (e) => {
+    setAge(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    if(age < 25 || age > 100){
+      alert('Please enter Valid Age')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p>First Name:<input required type="text" value={fName} onChange={handleUpdateFirstName} /></p>
+      <p>Last Name:<input type="text" value={lName} onChange={HandleUpdateLastName} /></p>
+      Age: <input type="text" value={age} onChange={handleUpdateAge} />
+      <select>
+        <option>1</option>
+        <option>2</option>
+      </select>
+
+      {/* <p>{`Your name is: ${fName} ${lName}`}</p> */}
+
+      <button onClick={handleSubmit}>Submit</button>
+
+      <ShowName fName={fName} lName={lName}/>
+     
     </div>
   );
 }
---
+++ src/showName.jsx
@@ -0,0 +1,7 @@
+const ShowName = (props) =>{ 
+    return(
+        <div>  <p>{`Your name is: ${props.fName} ${props.lName}`}</p></div>
+    )
+} 
+
+export default ShowName;
